3f9d4a398b5f5a605ab51d34a5e9b1c8420071d8
m-r-khan <abuzarkhan2010@gmail.com>
2023-04-20 11:39:18 +0530

    Ep 3

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
-    const parent = React.createElement("div",{id:"parent"},React.createElement("div",
-                                {id:"child"},[React.createElement("h1",{},"H1 tag"),
-                                React.createElement("h2",{},"H2 tag")]));
-    
-    
-    const heading = React.createElement("h1", {id:"heading"}, "Hello Guys");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+//const heading = React.createElement("h1", {id:"heading"}, "Hello Buddy");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
 
+const jsxHeading = (<h1 id="heading"> This is JSX</h1>); 
+root.render(jsxHeading);
+
+const Heady = () =>
+(
+    <h1 className="head">Second one</h1>
+);
+const heady1 = 
+(
+    <h1 className="head1">Third one</h1>
+);
+
+const number = 100;
+
+const FunctionalComponent = () =>
+(
+    <div id="container">
+        <Heady /> 
+        {number} 
+        <h2>{number}</h2>
+        {heady1}
+        <h1 className="heading">Functional Component</h1>
+    </div>
+    
+);
+
+root.render(<FunctionalComponent/> );
